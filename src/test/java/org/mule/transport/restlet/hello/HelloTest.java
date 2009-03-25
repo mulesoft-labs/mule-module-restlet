de566c53ca8c8625d0cc5df6b6b1d058ae9f0582
dandiep <dandiep@cdcfac1a-7744-0410-b0e5-b1db7e7324ef>
2009-03-25 17:51:43 +0000

    Update ports and lock down to mule 2.2.1
    git-svn-id: http://svn.muleforge.org/restlet/trunk@69 cdcfac1a-7744-0410-b0e5-b1db7e7324ef

--- src/test/java/org/mule/transport/restlet/hello/HelloTest.java
+++ src/test/java/org/mule/transport/restlet/hello/HelloTest.java
@@ -17,28 +17,28 @@ public class HelloTest extends FunctionalTestCase {
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, "GET");
         props.put(HelloWorldResource.X_CUSTOM_HEADER, "foo");
         
-        MuleMessage result = client.send("http://localhost:9002/bar/foo", "test", props);
+        MuleMessage result = client.send("http://localhost:63081/bar/foo", "test", props);
         assertEquals("hello, world", result.getPayloadAsString());
         assertEquals("foo", result.getStringProperty(HelloWorldResource.X_CUSTOM_HEADER, ""));
         
-        result = client.send("http://localhost:9002/bar/foo?name=Dan", "test", props);
+        result = client.send("http://localhost:63081/bar/foo?name=Dan", "test", props);
         assertEquals("hello Dan", result.getPayloadAsString());
 
-        result = client.send("http://localhost:9002/bar/foo?name=Mr. XML", "test", props);
+        result = client.send("http://localhost:63081/bar/foo?name=Mr. XML", "test", props);
         assertEquals("<hello>Mr. XML</hello>", result.getPayloadAsString());
         assertEquals("application/xml", result.getProperty("Content-Type"));
         
         // try custom status codes
         props.put(HelloWorldResource.X_STATUS_HEADER, "201");
         
-        result = client.send("http://localhost:9002/bar/foo", "test", props);
+        result = client.send("http://localhost:63081/bar/foo", "test", props);
         assertEquals("hello, world", result.getPayloadAsString());
         assertEquals("201", result.getProperty(HttpConnector.HTTP_STATUS_PROPERTY));
         
 
         props.put(HttpConnector.HTTP_METHOD_PROPERTY, "HEAD");
         props.put(HelloWorldResource.X_STATUS_HEADER, "204");
-        result = client.send("http://localhost:9002/bar/foo", "test", props);
+        result = client.send("http://localhost:63081/bar/foo", "test", props);
         assertEquals("foo", result.getStringProperty(HelloWorldResource.X_CUSTOM_HEADER, ""));
         assertEquals("204", result.getProperty(HttpConnector.HTTP_STATUS_PROPERTY));
     }
